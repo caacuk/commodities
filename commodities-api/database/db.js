ba1f7960b4ce39774ac9062664579cfadde6fbd1
caacuk <caacuk@gmail.com>
2020-09-02 07:51:07 +0700

    Update db.js

--- commodities-api/database/db.js
+++ commodities-api/database/db.js
@@ -1,8 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv/config");
 const db = {};
-const stringConnection =
-  "postgres://nelbgfvhugqhgv:eee0e1693abad87019c44deb2f61d4a3fadb78b5490bd441b47933079adadba8@ec2-54-197-254-117.compute-1.amazonaws.com:5432/d74b4uajbmmr62";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
